3e847c6ef2fe3a3f17fecf7aab0c61fb24ef56e9
Metalocif <62672020+Metalocif@users.noreply.github.com>
2023-11-16 14:33:41 +0100

    Added 8 prefixes, fixes, mod options
    See patch notes for v1.1

--- scripts/evolutions.lua
+++ scripts/evolutions.lua
@@ -1,59 +1,195 @@
 function IsPrefixValidForVek(prefix, vekType)
+	local options = mod_loader.currentModContent[mod.id].options
+	if options["Enable"..prefix] and not options["Enable"..prefix].enabled then return false end
 	if _G[vekType].Prefixed then return false end									  --prevents double-prefixing
 	if prefix == "Stable" and not _G[vekType].Pushable then return false end
 	if prefix == "Fireproof" and _G[vekType].IgnoreFire then return false end
-	--if prefix == "Acidproof" and _G[vekType].IgnoresAcid then return false end	  --not sure it's a thing
 	if prefix == "Smokeproof" and _G[vekType].IgnoreSmoke then return false end
-	-- if prefix == "Freezeproof" and _G[vekType].IgnoresFreeze then return false end --not a thing
 	if prefix == "Leaping" and (_G[vekType].Jumper or _G[vekType].Flying or _G[vekType].Burrows) then return false end
 	if prefix == "Armored" and _G[vekType].Armor then return false end
-	if prefix == "Heavy" and (_G[vekType].Health > 7 or _G[vekType].MoveSpeed < 2) then return false end	--block on Psions? how to make it work with Vextra's?
+	if prefix == "Heavy" and (_G[vekType].Health > 7 or _G[vekType].MoveSpeed < 2 or _G[vekType].VoidShockImmune) then return false end
+	if prefix == "Light" and _G[vekType].Health == 1 then return false end
 	if prefix == "Volatile" and _G[vekType].Explodes then return false end
 	if prefix == "Massive" and _G[vekType].Massive then return false end
 	if prefix == "Undying" and _G[vekType].Corpse then return false end
+	if prefix == "Burrowing" and (_G[vekType].Jumper or _G[vekType].Flying or _G[vekType].Burrows or _G[vekType].Tier == TIER_BOSS or _G[vekType].Health < 3) then return false end
 	if prefix == "Ruinous" and _G[vekType].IsDeathEffect then return false end
 	if prefix == "Purifying" and _G[vekType].IsDeathEffect then return false end
 	if prefix == "Healing" and _G[vekType].IsDeathEffect then return false end
+	-- if prefix == "Webbing" and (_G[vekType].Ranged == 1 or _G[vekType].MoveSpeed < 3) then return false end
+	if prefix == "Splitting" and _G[vekType].MoveSpeed == 0 then return false end	
+	if prefix == "Oozing" and _G[vekType].MoveSpeed == 0 then return false end	
+	if prefix == "Infectious" and (_G[vekType].Ranged == 1 or _G[vekType].MoveSpeed < 3) then return false end	
+	if prefix == "Regenerating" and _G[vekType].Health == 1 then return false end
+	if prefix == "Wrathful" and _G[vekType].VoidShockImmune then return false end
+	if prefix == "Tyrannical" and not string.find(_G[vekType].Name, "Psion") then return false end
+	-- _G[vekType].VoidShockImmune is true for weaponless enemies, basically string.find(_G[vekType].Name, "Psion")/Blobber/Spider/...
 	return true
 end
 
 function CreateEvolvedVek(prefix, vekType)
-	local name = _G[vekType].Name
+	--local name = _G[vekType].Name
+	local name = PAWN_FACTORY:CreatePawn(vekType):GetMechName()	--used because in AE, the .Name field has wrong values, eg. Dung Beetle instead of Tumblebug
 	local portrait = "enemy/"..vekType
 	if _G[prefix..vekType] ~= nil then return true end
 	if prefix == "Stable" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, Pushable = false,} end
 	if prefix == "Fireproof" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, IgnoreFire = true,} end
-	-- if prefix == "Acidproof" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, IgnoresAcid = true,} end
 	if prefix == "Smokeproof" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, IgnoreSmoke = true,} end
 	if prefix == "Leaping" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, Jumper = true,} end
 	if prefix == "Armored" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, Armor = true,} end
 	if prefix == "Heavy" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, Health = _G[vekType].Health + 2, MoveSpeed = _G[vekType].MoveSpeed - 1,} end
+	if prefix == "Light" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, Health = _G[vekType].Health - 1, MoveSpeed = _G[vekType].MoveSpeed + 2,} end
 	if prefix == "Volatile" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, Explodes = true,} end
 	if prefix == "Massive" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, Massive = true,} end
 	if prefix == "Undying" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, Corpse = true,} end
+	if prefix == "Burrowing" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, Burrows = true,} end
 	if prefix == "Ruinous" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, IsDeathEffect = true, GetDeathEffect = RuinousDE} end
 	if prefix == "Purifying" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, IsDeathEffect = true, GetDeathEffect = PurifyingDE} end
 	if prefix == "Healing" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, IsDeathEffect = true, GetDeathEffect = HealingDE} end
+	-- if prefix == "Webbing" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, GetWeapon = Webbing,} end
+	-- GetWeapon triggers at start of movement so I can't web things at end of movement, big sad
+	if prefix == "Splitting" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, GetWeapon = Splitting,} end
+	if prefix == "Oozing" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, GetWeapon = Oozing,} end
+	if prefix == "Infectious" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, GetWeapon = Infectious,} end
+	if prefix == "Regenerating" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, GetWeapon = Regenerating,} end
+	if prefix == "Wrathful" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, GetWeapon = Wrathful,} end
+	if prefix == "Tyrannical" then _G[prefix..vekType] = _G[vekType]:new{Name = prefix.." "..name, Prefixed = true, Portrait = portrait, Ranged = 1, SkillList = {"TyrannicalAtk1"}, Tooltip = ""} end
 	return false
 end
 
+function Infectious()
+	for i = DIR_START, DIR_END do
+		local curr = Pawn:GetSpace() + DIR_VECTORS[i]
+		local target = Board:GetPawn(curr)
+		if target and target:IsPlayer() then 
+			if target:IsInfected() then Board:DamageSpace(curr, 1) else target:SetInfected(true) end
+		end
+	end
+	return 1
+end
+
+function Regenerating()
+	Board:DamageSpace(Pawn:GetSpace(), -1)
+	return 1
+end
+
+function Wrathful()
+	Pawn:SetBoosted(true)
+	return 1
+end
+
+-- function Webbing()
+	-- modApi:conditionalHook(
+		-- function()
+			-- return Pawn and Pawn:IsQueued() and string.find(Pawn:GetType(), "Webbing") 
+		-- end,
+		-- function()
+			-- LOG("start webbing from "..Pawn:GetSpace():GetString())
+			-- ret = SkillEffect()
+			-- for i = DIR_START, DIR_END do
+				-- local curr = Pawn:GetSpace() + DIR_VECTORS[i]
+				-- Board:Ping(curr, COLOR_BLACK)
+				-- if Board:GetPawn(curr) and Board:GetPawn(curr):IsPlayer() then 
+					-- LOG("found something in "..curr:GetString())
+					-- ret:AddGrapple(Pawn:GetSpace(), curr, "hold")
+				-- end
+			-- end
+			-- Board:AddEffect(ret)
+		-- end
+	-- )
+	-- return 1
+-- end
+
+function Splitting()
+	--we'll only spawn something if we are not above liquid/chasm, we could move to something adjacent, and we wouldn't move into fire/landmine
+	--we have to move somewhere adjacent because I want the spawn's position to be the previous position of the spawner
+	--but it's possible the spawner will move 0 tiles and I can't know this
+	--I guess I could overwrite GetPositionScore but this is easier, also pawn could be stuck
+	local startPos = Pawn:GetSpace()
+	if Pawn:GetTurnCount() > 1 and Board:GetTerrain(startPos) ~= TERRAIN_WATER and Board:GetTerrain(startPos) ~= TERRAIN_HOLE then 
+		local adjacents = extract_table(Board:GetReachable(startPos, 1, Pawn:GetPathProf()))
+		for i = 1, #adjacents do
+			if not (Board:IsFire(adjacents[i]) or Board:IsDangerousItem(adjacents[i])) then
+				local ret = SkillEffect()
+				ret:AddMove(Board:GetPath(startPos, adjacents[i], Pawn:GetPathProf()), NO_DELAY)
+				Board:AddEffect(ret)
+				Board:AddPawn("Blob1", startPos)
+				break
+			end
+		end
+	end
+	return 1
+end
+
+function Oozing()
+	local startPos = Pawn:GetSpace()
+	if Pawn:GetTurnCount() > 1 and Board:GetTerrain(startPos) ~= TERRAIN_WATER and Board:GetTerrain(startPos) ~= TERRAIN_HOLE then 
+		local adjacents = extract_table(Board:GetReachable(startPos, 1, Pawn:GetPathProf()))
+		for i = 1, #adjacents do
+			if not (Board:IsFire(adjacents[i]) or Board:IsDangerousItem(adjacents[i])) then
+				local ret = SkillEffect()
+				ret:AddMove(Board:GetPath(startPos, adjacents[i], Pawn:GetPathProf()), NO_DELAY)
+				Board:AddEffect(ret)
+				Board:AddPawn("BlobBossSmall", startPos)
+				break
+			end
+		end
+	end
+	return 1
+end
+
+TyrannicalAtk1 = LineArtillery:new{
+	Name="Hive Targeting",
+	Description="Strikes a target with tentacles. Will prioritize units over buildings.",
+	ArtillerySize = 5,
+	Damage = 1,
+	Type = 1,
+	ScoreEnemy = 6,
+	Class = "Enemy",
+	Icon = "weapons/enemy_scarab1.png",
+	Projectile = "",
+	Explosion = "",
+	ImpactSound = "",
+	TipImage = {
+		Unit = Point(2,4),
+		Enemy = Point(2,1),
+		Building = Point(2,2),
+		Target = Point(2,1),
+		CustomPawn = "Jelly_Health1"
+	}
+}
+
+function TyrannicalAtk1:GetSkillEffect(p1, p2)
+	--stolen from Generic's Secret Squad 2, go play it, very fun
+	local ret = SkillEffect()
+	local Tanim1 = SpaceDamage(p2,1)--the tentacles attacking from the front
+	local Tanim2 = SpaceDamage(p2,0)--the tentacles attacking from the back
+	Tanim1.sAnimation ="PsionAttack_Front"
+	Tanim2.sAnimation ="PsionAttack_Back"
+	ret:AddQueuedBounce(p2,3)
+	ret:AddQueuedArtillery(Tanim1, "", NO_DELAY)
+	ret:AddQueuedDamage(Tanim2)
+	return ret
+end
+
+
 function GeneratePrefix(pawn)
 	local tile = pawn:GetSpace()
+	if string.find(pawn:GetMechName(), "Psion") and pawn:GetType() ~= "Jelly_Lava1" then return "Tyrannical" end 	--hardcoded but Tyrants should basically never be prefixed anyway
 	if pawn:IsFire() and IsPrefixValidForVek("Fireproof", pawn:GetType()) then return "Fireproof" end
 	if Board:IsEdge(tile) and IsPrefixValidForVek("Stable", pawn:GetType()) then return "Stable" end
 	if Board:IsSmoke(tile) and IsPrefixValidForVek("Smokeproof", pawn:GetType()) then return "Smokeproof" end
-	-- local wasBlocked = true
-	-- for i = DIR_START, DIR_END do
-		-- if not Board:IsBlocked(tile, pawn:GetPathProf()) then wasBlocked = false end
-	-- end
-	-- if wasBlocked and IsPrefixValidForVek("Leaping", pawn:GetType()) then return "Leaping" end
-	--if we couldn't find anything 'logical', just pick something at random
-	local prefixes = {"Stable","Fireproof","Smokeproof","Leaping","Armored","Heavy","Volatile","Massive","Undying","Ruinous","Purifying","Healing"}
+	if pawn:GetMutation() == 4 and IsPrefixValidForVek("Regenerating", pawn:GetType()) then return "Regenerating" end
+	if pawn:GetMutation() == 5 and IsPrefixValidForVek("Armored", pawn:GetType()) then return "Armored" end
+	if pawn:GetMutation() == 6 and IsPrefixValidForVek("Volatile", pawn:GetType()) then return "Volatile" end	
+	if pawn:GetMutation() == 7 and IsPrefixValidForVek("Wrathful", pawn:GetType()) then return "Wrathful" end
+	if GetCurrentMission() == Mission_BlobBoss and IsPrefixValidForVek("Oozing", pawn:GetType()) then return "Oozing" end
+	local prefixes = {"Stable","Fireproof","Smokeproof","Leaping","Armored","Heavy","Light","Volatile","Massive","Undying","Burrowing","Ruinous","Purifying","Healing","Splitting","Oozing","Infectious","Regenerating","Wrathful"}
 	local prefix
 	repeat
 		prefix = prefixes[math.random(#prefixes)]
 	until IsPrefixValidForVek(prefix, pawn:GetType())
-	return prefix
+	return "Stable" --prefix
 end
 
 function AreSameFamily(type1, type2)
@@ -93,20 +229,21 @@ end
 
 local function HOOK_MissionStart(mission)
 	if GAME.EvolvedVeks == nil then return false end
-	while Board:IsBusy() do
-	end
-	local delayDamage = SpaceDamage(Point(0, 0))
-	delayDamage.fDelay = 2
-	Board:AddEffect(delayDamage)
+	local options = mod_loader.currentModContent[mod.id].options
+	if options["PrefixStartCount"] and options["PrefixStartCount"] == 0 then return false end
+	local prefixesApplied = 0
 	for _, tile in ipairs(Board) do
 		local pawn = Board:GetPawn(tile)
 		if pawn and pawn:GetTeam() == TEAM_ENEMY and pawn:GetTeam() ~= TEAM_BOTS then
 			for i = 1, #GAME.EvolvedVeks do
-				if GAME.EvolvedVeks[i].Type == pawn:GetType() and GAME.EvolvedVeks[i].Remaining > 0 then
+				if _G[GAME.EvolvedVeks[i].Prefix..GAME.EvolvedVeks[i].Type].Name == "Missing Mod" then Board:Ping(tile, COLOR_BLACK) LOG("prevented missing mod for "..GAME.EvolvedVeks[i].Prefix..GAME.EvolvedVeks[i].Type) end
+				if GAME.EvolvedVeks[i].Type == pawn:GetType() and GAME.EvolvedVeks[i].Remaining > 0 and _G[GAME.EvolvedVeks[i].Prefix..GAME.EvolvedVeks[i].Type].Name ~= "Missing Mod" then
 					Board:RemovePawn(tile)
 					Board:AddPawn(GAME.EvolvedVeks[i].Prefix..pawn:GetType(), tile)
+					Board:GetPawn(tile):SpawnAnimation()
 					GAME.EvolvedVeks[i].Remaining = GAME.EvolvedVeks[i].Remaining - 1
-					break
+					prefixesApplied = prefixesApplied + 1
+					if options["PrefixStartCount"] and options["PrefixStartCount"] == prefixesApplied then return true end
 				end
 			end
 		end
@@ -115,10 +252,16 @@ end
 
 local function HOOK_VekSpawnAdded(mission, spawnData)
 	if GAME.EvolvedVeks == nil then return false end
+	local options = mod_loader.currentModContent[mod.id].options
+	if options["PrefixSpawns"] and not options["PrefixSpawns"].enabled then return false end
 	for i = 1, #GAME.EvolvedVeks do
 		if GAME.EvolvedVeks[i].Type == spawnData.type and GAME.EvolvedVeks[i].Remaining > 0 then
 			--mission:ModifySpawnPoint(spawnData.location, {location = spawnData.location, type = GAME.EvolvedVeks[i].Prefix..GAME.EvolvedVeks[i].Type})
-			Mission:ChangeSpawnPointPawnType(spawnData.location, GAME.EvolvedVeks[i].Prefix..GAME.EvolvedVeks[i].Type)
+			mission:ChangeSpawnPointPawnType(spawnData.location, GAME.EvolvedVeks[i].Prefix..GAME.EvolvedVeks[i].Type)
+			-- mission:RemoveSpawnPoint(spawnData.location)
+			-- mission:SpawnPawn(spawnData.location, GAME.EvolvedVeks[i].Prefix..GAME.EvolvedVeks[i].Type)	--or spawnPawnInternal?
+			-- self.Board:SpawnPawn(pawn, location)
+			-- addSpawnData(self, location, pawn:GetType(), pawn:GetId())
 			GAME.EvolvedVeks[i].Remaining = GAME.EvolvedVeks[i].Remaining - 1
 			break
 		end
@@ -128,7 +271,7 @@ end
 local function HOOK_MissionEnd(mission, ret)
 	for _, tile in ipairs(Board) do
 		local pawn = Board:GetPawn(tile)
-		if pawn and pawn:GetTeam() == TEAM_ENEMY and pawn:GetTeam() ~= TEAM_BOTS and not _G[pawn:GetType()].Prefixed then
+		if pawn and pawn:GetTeam() == TEAM_ENEMY and pawn:GetTeam() ~= TEAM_BOTS and not _G[pawn:GetType()].Prefixed and not pawn:IsFrozen() then
 			local prefix = GeneratePrefix(pawn)
 			CreateEvolvedVek(prefix, pawn:GetType())
 			if GAME.EvolvedVeks == nil then GAME.EvolvedVeks = {} end
@@ -137,15 +280,16 @@ local function HOOK_MissionEnd(mission, ret)
 				if GAME.EvolvedVeks[i].Type == pawn:GetType() and GAME.EvolvedVeks[i].Prefix == prefix then GAME.EvolvedVeks[i].Remaining = GAME.EvolvedVeks[i].Remaining + 1 found = true break end
 			end
 			if not found then GAME.EvolvedVeks[#GAME.EvolvedVeks+1] = {Type = pawn:GetType(), Prefix = prefix, Remaining = 1} end
-			--LOG("added a "..prefix.." "..pawn:GetType()..", we stored "..tostring(#GAME.EvolvedVeks).." different Vek.")
+			LOG("added a "..prefix.." "..pawn:GetType()..", we stored "..tostring(#GAME.EvolvedVeks).." different Vek.")
 		end
 	end
 end
 
 local function HOOK_PostLoadGame()
-	if GAME.EvolvedVeks == nil then LOG("no table") else LOG("remaking "..tostring(#GAME.EvolvedVeks).." prefixed Veks") end
+	-- if GAME.EvolvedVeks == nil then LOG("no table") else LOG("remaking "..tostring(#GAME.EvolvedVeks).." prefixed Veks") end
+	if GAME.EvolvedVeks == nil then LOG("no table") return false end
 	for i = 1, #GAME.EvolvedVeks do
-		CreateEvolvedVek(GAME.EvolvedVeks[i].Prefix, GAME.EvolvedVeks[i].Type)
+		-- CreateEvolvedVek(GAME.EvolvedVeks[i].Prefix, GAME.EvolvedVeks[i].Type)
 		if not CreateEvolvedVek(GAME.EvolvedVeks[i].Prefix, GAME.EvolvedVeks[i].Type) then
 			LOG("added a "..GAME.EvolvedVeks[i].Prefix.." "..GAME.EvolvedVeks[i].Type..", that is the "..tostring(i).." we stored.")
 		end
--- scripts/init.lua
+++ scripts/init.lua
@@ -2,10 +2,12 @@ local description = "Adds prefixes to enemies. Each enemy can have a random buff
 local mod = {
 	id = "Meta_EvolvingVek",
 	name = "Evolving Vek",
-	version = "1.0",
+	version = "1.1",
 	requirements = {},
 	dependencies = { 
 		modApiExt = "1.18", --We can get this by using the variable `modapiext`
+		memedit = "1.0.2",
+		easyEdit = "2.0.4",
 	},
 	modApiVersion = "2.9.1",
 	icon = "img/mod_icon.png",
@@ -18,6 +20,143 @@ end
 
 function mod:load( options, version)
 	mod.icon = self.resourcePath .."img/mod_icon.png"
+end
+
+function mod:metadata()
+	modApi:addGenerationOption(
+		"PrefixSpawns",
+		"Modify spawns during mission",
+		"Check to modify spawns during missions instead of solely before deployment; this is currently buggy (default: true).",
+		{ enabled = true }
+	)
+	modApi:addGenerationOption(
+		"PrefixStartCount",
+		"Max prefixes at start of mission",
+		"Choose how many Vek can have a prefix on the first turn (default: 1).",
+		{
+			values = { 1, 2, 3, 4, 5},
+		}
+	)
+	modApi:addGenerationOption(
+		"Enable_Stable",
+		"Enable the Stable prefix",
+		"Check to allow Vek to be given the Stable prefix, making them immune to pushes (default: true).",
+		{ enabled = true }
+	)
+	modApi:addGenerationOption(
+		"Enable_Fireproof",
+		"Enable the Fireproof prefix",
+		"Check to allow Vek to be given the Fireproof prefix, making them immune to fire (default: true).",
+		{ enabled = true }
+	)
+	modApi:addGenerationOption(
+		"Enable_Smokeproof",
+		"Enable the Smokeproof prefix",
+		"Check to allow Vek to be given the Smokeproof prefix, making them immune to smoke (default: true).",
+		{ enabled = true }
+	)
+	modApi:addGenerationOption(
+		"Enable_Leaping",
+		"Enable the Leaping prefix",
+		"Check to allow Vek to be given the Leaping prefix, making them move by leaping (default: true).",
+		{ enabled = true }
+	)
+	modApi:addGenerationOption(
+		"Enable_Armored",
+		"Enable the Armored prefix",
+		"Check to allow Vek to be given the Armored prefix, granting them armor (default: true).",
+		{ enabled = true }
+	)
+	modApi:addGenerationOption(
+		"Enable_Heavy",
+		"Enable the Heavy prefix",
+		"Check to allow Vek to be given the Heavy prefix, granting them +2 HP at the cost of 1 movement (default: true).",
+		{ enabled = true }
+	)
+	modApi:addGenerationOption(
+		"Enable_Light",
+		"Enable the Light prefix",
+		"Check to allow Vek to be given the Light prefix, granting them +2 movement at the cost of 1 HP (default: true).",
+		{ enabled = true }
+	)
+	modApi:addGenerationOption(
+		"Enable_Volatile",
+		"Enable the Volatile prefix",
+		"Check to allow Vek to be given the Volatile prefix, making them explode when killed (default: true).",
+		{ enabled = true }
+	)
+	modApi:addGenerationOption(
+		"Enable_Massive",
+		"Enable the Massive prefix",
+		"Check to allow Vek to be given the Massive prefix, making them immune to death by drowning (default: true).",
+		{ enabled = true }
+	)
+	modApi:addGenerationOption(
+		"Enable_Undying",
+		"Enable the Undying prefix",
+		"Check to allow Vek to be given the Undying prefix, making them leave a corpse when killed (default: true).",
+		{ enabled = true }
+	)
+	modApi:addGenerationOption(
+		"Enable_Burrowing",
+		"Enable the Burrowing prefix",
+		"Check to allow Vek to be given the Burrowing prefix, making them move by burrowing and burrow when they take damage (default: true).",
+		{ enabled = true }
+	)
+	modApi:addGenerationOption(
+		"Enable_Ruinous",
+		"Enable the Ruinous prefix",
+		"Check to allow Vek to be given the Ruinous prefix, making them crack their tile when killed (default: true).",
+		{ enabled = true }
+	)
+	modApi:addGenerationOption(
+		"Enable_Purifying",
+		"Enable the Purifying prefix",
+		"Check to allow Vek to be given the Purifying prefix, making them remove A.C.I.D. from adjacent tiles when killed (default: true).",
+		{ enabled = true }
+	)
+	modApi:addGenerationOption(
+		"Enable_Healing",
+		"Enable the Healing prefix",
+		"Check to allow Vek to be given the Healing prefix, making them heal adjacent Veks when killed (default: true).",
+		{ enabled = true }
+	)
+	modApi:addGenerationOption(
+		"Enable_Splitting",
+		"Enable the Splitting prefix",
+		"Check to allow Vek to be given the Splitting prefix, making them leave a blob at the start of movement after their first turn alive (default: true).",
+		{ enabled = true }
+	)
+	modApi:addGenerationOption(
+		"Enable_Oozing",
+		"Enable the Oozing prefix",
+		"Check to allow Vek to be given the Oozing prefix, making them leave a small goo at the start of movement after their first turn alive (default: true).",
+		{ enabled = true }
+	)
+	modApi:addGenerationOption(
+		"Enable_Infectious",
+		"Enable the Infectious prefix",
+		"Check to allow Vek to be given the Infectious prefix, making them spread Vek mites on allied units at the start of movement and damaging infected units (default: true).",
+		{ enabled = true }
+	)
+	modApi:addGenerationOption(
+		"Enable_Regenerating",
+		"Enable the Regenerating prefix",
+		"Check to allow Vek to be given the Regenerating prefix, making them heal 1 HP at the start of movement (default: true).",
+		{ enabled = true }
+	)
+	modApi:addGenerationOption(
+		"Enable_Wrathful",
+		"Enable the Wrathful prefix",
+		"Check to allow Vek to be given the Wrathful prefix, making them boosted at the start of movement (default: true).",
+		{ enabled = true }
+	)
+	modApi:addGenerationOption(
+		"Enable_Tyrannical",
+		"Enable the Tyrannical prefix",
+		"Check to allow Psions to be given the Tyrannical prefix, granting them a weak artillery attack (default: true).",
+		{ enabled = true }
+	)
 end
 
 return mod
